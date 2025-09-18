cc2113bed9b55ec253879e63542955379ccb891a
Soli <soli@gmail.com>
2025-09-18 21:47:04 +0330

    fix : change some space on navigation items

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -4,7 +4,7 @@ import SaveButton from "./button/SaveButton.jsx";
 
 const Navigation = () => {
   return (
-    <nav className="flex justify-between h-14 p-2 bg-gray-950">
+    <nav className="flex justify-between h-16 p-2 bg-gray-950">
       <ProjectTitle />
       <SaveButton />
     </nav>
