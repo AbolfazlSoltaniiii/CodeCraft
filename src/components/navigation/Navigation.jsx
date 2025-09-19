b629098fb6bfe0eac54ddae7d9d7e9c159c0c5ea
Soli <soli@gmail.com>
2025-09-19 21:00:56 +0330

    refactor : using reducer instead of useState to management states

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import ProjectTitle from "./projectTitle/ProjectTitle.jsx";
 import SaveButton from "./button/SaveButton.jsx";
 
-const Navigation = ({title, updateTitle, onSaveClick}) => {
+const Navigation = ({title, dispatch, onSaveClick}) => {
   return (
     <nav className="flex justify-between h-16 p-2 bg-gray-950">
-      <ProjectTitle title={title} updateTitle={updateTitle} />
+      <ProjectTitle title={title} dispatch={dispatch} />
       <SaveButton onSaveClick={onSaveClick} />
     </nav>
   );
